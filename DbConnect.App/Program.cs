780d2d0d1048cd937da08056fb53d7ab39315743
Kevin <kevin.michael.horan@gmail.com>
2018-04-08 16:48:49 -0700

    remove some unused usings

--- DbConnect.App/Program.cs
+++ DbConnect.App/Program.cs
@@ -1,9 +1,6 @@
 ﻿using DbConnect.Data;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DbConnect.App
 {
--- DbConnect.Data/BlogRepository.cs
+++ DbConnect.Data/BlogRepository.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DbConnect.Data
 {
@@ -20,6 +17,8 @@ namespace DbConnect.Data
                 dbContext.SaveChanges();
             }
         }
+
+
 
         public IEnumerable<BlogSet> GetBlogs()
         {
