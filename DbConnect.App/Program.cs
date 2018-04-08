913dcd182667764c2d6911654d8b1ff81a4b56c5
Kevin <kevin.michael.horan@gmail.com>
2018-04-08 08:54:41 -0700

    first commit

---
+++ DbConnect.App/Program.cs
@@ -0,0 +1,27 @@
+﻿using DbConnect.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbConnect.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Looking For Blogs...\n");
+            var blogRepo = new BlogRepository();
+            IEnumerable<BlogSet> blogs = blogRepo.GetBlogs();
+
+            foreach(var blog in blogs)
+            {
+                Console.WriteLine("Found Blog : " + blog.BlogTitle + " !\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DbConnect.Data/BlogRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbConnect.Data
+{
+    public class BlogRepository
+    {
+        public IEnumerable<BlogSet> GetBlogs()
+        {
+            using(var dbConext = new MyDb_20180408Entities())
+            {
+                List<BlogSet> blogs = dbConext.BlogSet.ToList();
+                return blogs;
+            }
+        }
+    }
+}
